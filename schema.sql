d20684f686deb43095ff0a5ec744865ba697e281
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2013-02-11 17:53:30 -0500

    Add SQL for simple stored proc.

--- schema.sql
+++ schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE hopes
   "date" VARCHAR(128),
   bumpCount INT
 );
+
+CREATE PROCEDURE avg_bump_count_func(OUT ans DOUBLE)
+  LANGUAGE javascript PARAMETER STYLE variables AS $$
+    function avg_bump_count() {
+      var conn = java.sql.DriverManager.getConnection("jdbc:default:connection", "test", "");
+      var ps = conn.prepareStatement("SELECT avg(bumpcount) FROM hopes");
+      var rs = ps.executeQuery();
+      rs.next();
+      return rs.getDouble(1);
+    }
+    ans = avg_bump_count();
+  $$;
